7fa65dab2dcad8b5792350fcfb8c09b4cbcb9568
Joshua Adams <joshadams554@gmail.com>
2018-10-01 14:34:30 -0400

    did step 4, added a bunch of stuff in Store

--- scripts/store.js
+++ scripts/store.js
@@ -11,7 +11,41 @@ const store = (function () {
   let hideCheckedItems = false;
   let searchTerm = '';
   let foo = 'bar'; 
-  return {items, hideCheckedItems, searchTerm}; 
+  let findById = function(id){
+    return items.find(item => item.id === id);
+  };
+  let addItem = function(name){
+    try {
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  let findAndUpdateName = function(id, newName){
+    try {
+      Item.validateName(newName);
+      this.findById(id).name = newName;
+    } catch (error) {
+      console.log(`Cannot update name: ${error.message}`);
+    }
+  }
+
+  let findAndToggleChecked = function(id){
+    this.findById(id).checked = !this.findById(id).checked;
+  };
+
+  let findAndDelete = function(id){
+
+    this.items = this.items.filter(item => id !== this.findById(item.id).id);
+
+  };
+
+  return {items, hideCheckedItems, searchTerm, addItem, findAndToggleChecked, findAndDelete, findAndUpdateName, findById}; 
 }()); 
+
+
 
 console.log(store);
